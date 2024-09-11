80485acebc254094062f7144f1f14f0866b3da0a
melloware <mellowaredev@gmail.com>
2024-09-11 16:16:26 -0400

    Initial commit

--- deployment/src/test/java/io/quarkiverse/mdns/test/MdnsDevModeTest.java
+++ deployment/src/test/java/io/quarkiverse/mdns/test/MdnsDevModeTest.java
@@ -13,7 +13,7 @@ public class MdnsDevModeTest {
     // Start hot reload (DevMode) test with your extension loaded
     @RegisterExtension
     static final QuarkusDevModeTest devModeTest = new QuarkusDevModeTest()
-        .setArchiveProducer(() -> ShrinkWrap.create(JavaArchive.class));
+            .setArchiveProducer(() -> ShrinkWrap.create(JavaArchive.class));
 
     @Test
     public void writeYourOwnDevModeTest() {
